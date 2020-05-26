550145fc8c42b190bae7a4ce0def816d5b781ffa
PontVergesJuanPedro <110959@tecnicatura.frc.utn.edu.ar>
2020-05-26 14:53:00 -0300

    mejoras en el login

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,33 +1,36 @@
 <template>
     <v-container
     class="fondo"
-    fill-height>
-        <v-row>
+    fill-height
+    align-content-space-around>
+        <v-row 
+        class="d-flex justify-start mb-5">
             <v-col 
             class="title text-center white--text"
             cols="12">
             <h2>Nombre</h2>
             </v-col>
+        </v-row>
+        <v-row>
             <v-col
-            class="white--text title"
+            class="white--text title mb-5"
             cols="12">
-            <h3>Login</h3>
+            <h3 class="font-weight-bold">Login</h3>
             <p class=" subtitle-1 ">Porfavor inicie sesión para continuar</p>
             </v-col>
-            <v-col cols="12">
+            <v-col 
+            cols="12">
                 <v-text-field
                   label="Email"
                   prepend-icon="mdi-email"
                 ></v-text-field>
-            </v-col>
-            <v-col cols="12">
-                <v-text-field
+                 <v-text-field
                   label="Password"
                   prepend-icon="mdi-lock"
                   append-outer-icon="mdi-help"
                 ></v-text-field>
             </v-col>
-                </v-row>
+        </v-row>
     </v-container>
 </template>
 
