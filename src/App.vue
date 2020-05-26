b34e7e5f5aede9cef53db43852894bf5f5c0b52b
PontVergesJuanPedro <110959@tecnicatura.frc.utn.edu.ar>
2020-05-26 17:47:10 -0300

    cambios al login

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,24 @@
 <template>
   <v-app>
-    <v-app-bar bottom v-if="this.$store.state.logged"  color="black"></v-app-bar>
+    <v-app-bar app v-if="this.$store.state.logged" bottom color="white">
+      <v-btn icon class="mx-3">
+        <v-icon large>mdi-home</v-icon>
+      </v-btn>
+      <v-btn icon  class="mx-3">
+        <v-icon large>mdi-book-open-variant</v-icon>
+      </v-btn>
+      <v-spacer></v-spacer>
+      <v-btn icon fab class="mb-10">
+        <v-icon x-large color="white" class="mb-2 boton px-7 py-7">mdi-plus</v-icon>
+      </v-btn>
+      <v-spacer></v-spacer>
+      <v-btn icon class="mx-3">
+        <v-icon large>mdi-calendar</v-icon>
+       </v-btn>
+      <v-btn icon class="mx-3">
+        <v-icon large>mdi-account</v-icon>
+      </v-btn>
+    </v-app-bar>
     <v-content>
         <router-view/>
     </v-content>
@@ -9,7 +27,6 @@
 
 <script>
 
-
 export default {
   name: "App",
   data: () => ({
@@ -17,3 +34,10 @@ export default {
   }),
 };
 </script>
+
+<style scoped>
+  .boton {
+    background-color:#FF9191;
+    border-radius: 100%;
+  }
+</style>
